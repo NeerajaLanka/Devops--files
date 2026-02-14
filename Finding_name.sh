e045750d69d25a3e6d23a85cc9c016b7e677c04f
neeraja <neeraja.eee@gmail.com>
2026-02-14 08:09:13 -0600

    only pushing Finding_name.sh

---
+++ Finding_name.sh
@@ -0,0 +1,10 @@
+# Bash script file #
+# Source - https://stackoverflow.com/a/2437466
+# Posted by Ignacio Vazquez-Abrams, modified by community. See post 'Timeline' for change history
+# Retrieved 2026-02-12, License - CC BY-SA 4.0
+#!/bin/bash
+search_dir=/Users/prasadkundeti/Downloads
+for Neeraja in "$search_dir"/*
+do      ``
+  echo "$Neeraja"
+done
