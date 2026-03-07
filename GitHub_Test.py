bb375eb91c3d8d1790ae967cf49bb543c36389dd
neeraja <neeraja.eee@gmail.com>
2026-03-06 21:38:07 -0600

    added github workflow

---
+++ GitHub_Test.py
@@ -0,0 +1,11 @@
+import os
+
+def main():
+    # Get a variable from GitHub Actions environment
+    user_name = os.getenv("USER_NAME", "Stranger")
+    
+    print(f"--- Hello, {user_name}! ---")
+    print("The Python script is running successfully in GitHub Actions.")
+
+if __name__ == "__main__":
+    main()
